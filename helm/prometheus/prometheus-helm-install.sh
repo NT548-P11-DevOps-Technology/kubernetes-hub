f51cad53708e40a5e5353da8fb58a0d9c86ff855
dangnguyen03 <dangnguyen.uit@gmail.com>
2024-12-09 20:26:28 +0700

    add: prometheus

--- helm/prometheus/prometheus-helm-install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-helm repo update prometheus-community
-helm install --namespace monitoring prometheus-stack -f values.yml prometheus-community/kube-prometheus-stack --create-namespace
